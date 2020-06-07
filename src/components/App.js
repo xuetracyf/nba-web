f83a63d6da3e71108af00dca4c8501bba6735aea
xuetracyf <xuetracyf@gmail.com>
2020-06-07 16:04:56 -0700

    nba react update

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import TopNavBar from './TopNavBar';
+import Main from './Main';
+import Profile from './Profile';
+import ShotChart from './ShotChart';
+
+function App() {
+ return (
+   <div className="App">
+     <TopNavBar />
+     <Main />
+   </div>
+ );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+import nba from '../nba-client';
+
+import Profile from './Profile';
+
+class Main extends Component {
+
+  state = {
+       playerInfo: {},
+       playerId: 201939
+   }
+
+   componentDidMount() {
+       window.nba = nba;
+       nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
+           .then((info) => {
+               console.log(info);
+               const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+               console.log(playInfo);
+               this.setState({ playerInfo: playInfo });
+           })
+   }
+
+   render() {
+       return (
+           <div className="main">
+               <Profile playerInfo={this.state.playerInfo} />
+           </div>
+       );
+   }
+}
+
+export default Main;
