3c237f0d7aaa7c3e3082238bb0995c64c0c133db
xuetracyf <xuetracyf@gmail.com>
2020-06-19 16:52:30 -0700

    version1.0 done

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,33 +8,46 @@ import ShotChart from './ShotChart';
 
 import DataViewContainer from './DataViewContainer';
 
-class Main extends Component {
+import SearchBar from './SearchBar';
 
-  state = {
-       playerInfo: {},
-       playerId: 201939
+import { DEFAULT_PLAYER_INFO } from '../constants';
+
+class Main extends Component {
+   state = {
+       playerInfo: DEFAULT_PLAYER_INFO
    }
 
    componentDidMount() {
        window.nba = nba;
-       nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
-           .then((info) => {
-               console.log(info);
-               const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-               console.log(playInfo);
-               this.setState({ playerInfo: playInfo });
-           })
+      this.loadPlayerInfo(DEFAULT_PLAYER_INFO.fullName);
+   }
+
+   loadPlayerInfo = (playerName) => {
+       nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId}).then((info) => {
+           console.log(info);
+           const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+           console.log(playInfo);
+           this.setState({ playerInfo: playInfo });
+       })
+   }
+
+   handleSelectPlayer = (playerName) => {
+       this.loadPlayerInfo(playerName);
    }
 
    render() {
-      return (
-          <div className="main">
-              <Profile playerInfo={this.state.playerInfo} />
-          
-              <DataViewContainer playerId={this.state.playerId}/>
-          </div>
-      );
+       return (
+           <div className="main">
+               <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
+               <div className="player">
+                   <Profile playerInfo={this.state.playerInfo} />
+                   <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+               </div>
+           </div>
+       );
    }
- }
+}
+
+
 
 export default Main;
